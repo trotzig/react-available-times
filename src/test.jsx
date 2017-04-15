0ca13328a3b3e2a772c81b0df94abd6559370b0a
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-15 22:10:49 +0200

    Add left sidebar to test page
    I want to make sure that the calendar stretches in its container. Having
    a sidebar will help me avoid using media-queries and will make it easier
    to test scrolling etc.

--- src/test.jsx
+++ src/test.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import AvailableTimes from './AvailableTimes.jsx';
-import styles from './reset.css';
+import styles from './test.css';
+
+import './reset.css';
 
 class Test extends Component {
   render() {
     return (
       <div className={styles.component}>
+        <div className={styles.intro}>
+          <h1>Example #1</h1>
+          <p>
+            Uses the current date as the starting point.
+          </p>
+        </div>
         <AvailableTimes />
       </div>
     );
