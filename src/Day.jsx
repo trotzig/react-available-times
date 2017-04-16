f2dd8b3ea960aa8dafad0c7caa9c5a1c1b1588ac
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-16 08:08:08 +0200

    Add ability to "paint" available times
    So far, just POC. You can paint one area per day.

--- src/Day.jsx
+++ src/Day.jsx
@@ -3,8 +3,51 @@ import React, { Component } from 'react';
 import hours from './hours';
 import styles from './Day.css';
 
+function relativeY(e) {
+  const scrollableAncestor = e.target.parentNode.parentNode;
+  return e.pageY - scrollableAncestor.offsetTop + scrollableAncestor.scrollTop;
+}
+
 export default class Day extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  handleMouseDown(e) {
+    const start = relativeY(e);
+    this.setState({
+      start,
+      recording: true,
+      end: start + 25,
+    })
+  }
+
+  handleMouseMove(e) {
+    if (!this.state.recording) {
+      return;
+    }
+    const end = relativeY(e);
+    this.setState({
+      end: Math.max(this.state.start + 25, end),
+    });
+  }
+
+  handleMouseUp(e) {
+    this.setState({
+      recording: false,
+    });
+  }
+
   render() {
+    const {
+      start,
+      end,
+    } = this.state;
+
     return (
       <div className={styles.component}>
         {hours.map((hour) => (
@@ -15,6 +58,23 @@ export default class Day extends Component {
             <div className={styles.halfHour}/>
           </div>
         ))}
+
+        {start > 0 && end > 0 && (
+          <div
+            className={styles.currentSelection}
+            style={{
+              top: start,
+              height: end - start,
+            }}
+          >
+          </div>
+        )}
+        <div
+          onMouseDown={this.handleMouseDown}
+          onMouseUp={this.handleMouseUp}
+          onMouseMove={this.handleMouseMove}
+          className={styles.mouseTarget}
+        />
       </div>
     );
   }
