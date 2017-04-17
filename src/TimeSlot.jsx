efb34c1512b0f8c700b8b611656d7a9f53302d14
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-17 20:50:22 +0200

    Flip <TimeSlot> labels when there is better room vertically
    This is an idea I've been wanting to explore for a bit. By rotating
    labels -90deg in narrow places, we can fit more text.

--- src/TimeSlot.jsx
+++ src/TimeSlot.jsx
@@ -1,34 +1,39 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
+import withAvailableWidth from 'react-with-available-width';
 
 import positionInDay from './positionInDay';
 import styles from './TimeSlot.css';
 import zeroPad from './zeroPad';
 
-export default class TimeSlot extends PureComponent {
-  renderLabel() {
-    const {
-      start,
-      end,
-    } = this.props;
-
-    return (
-      <span className={styles.label}>
-        {zeroPad(start.getHours())}:{zeroPad(start.getMinutes())}
-        {' - '}
-        {zeroPad(end.getHours())}:{zeroPad(end.getMinutes())}
-      </span>
-    );
+class TimeSlot extends PureComponent {
+  label() {
+    const { start, end } = this.props;
+    const from = `${zeroPad(start.getHours())}:${zeroPad(start.getMinutes())}`;
+    const to = `${zeroPad(end.getHours())}:${zeroPad(end.getMinutes())}`;
+    return `${from} - ${to}`;
   }
 
   render() {
     const {
       start,
       end,
+      availableWidth,
     } = this.props;
 
     const top = positionInDay(start);
     const bottom = positionInDay(end);
+
+    const height = bottom - top;
+
+    const labelClasses = [styles.label];
+    const labelStyle = {};
+    if (height > availableWidth && availableWidth < 60) {
+      labelClasses.push(styles.flip);
+      labelStyle.width = height;
+      labelStyle.marginLeft = -((height / 2) - 10);
+      labelStyle.marginTop = -10;
+    }
 
     return (
       <div
@@ -38,7 +43,12 @@ export default class TimeSlot extends PureComponent {
           height: bottom - top,
         }}
       >
-        {this.renderLabel()}
+        <div
+          className={labelClasses.join(' ')}
+          style={labelStyle}
+        >
+          {this.label()}
+        </div>
       </div>
     );
   }
@@ -47,4 +57,7 @@ export default class TimeSlot extends PureComponent {
 TimeSlot.propTypes = {
   start: PropTypes.instanceOf(Date).isRequired,
   end: PropTypes.instanceOf(Date).isRequired,
+  availableWidth: PropTypes.number.isRequired,
 }
+
+export default withAvailableWidth(TimeSlot);
