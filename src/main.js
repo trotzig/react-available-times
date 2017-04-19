ae09558a5cbe33b8832f00def531dfa3a1fee4dc
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-19 14:34:42 +0200

    Add commonjs library export
    We want to make the module easy to install via npm.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 import AvailableTimes from './AvailableTimes';
 
-export default function () {
-  console.log(AvailableTimes);
-}
+export default AvailableTimes;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const nodeExternals = require('webpack-node-externals');
+
+const rules = require('./webpack.rules.js');
+
+// This config file is used to create a commonjs bundle that we export.
+module.exports = {
+  entry: path.join(__dirname, 'src/main.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs',
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
+  module: {
+    rules,
+  },
+};
