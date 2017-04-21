bd13ad775c864cdf3de9d8032d2d3571897f07d6
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-21 10:04:53 +0200

    Use svg arrows for buttons in toolbar

--- src/AvailableTimes.jsx
+++ src/AvailableTimes.jsx
@@ -6,6 +6,20 @@ import addOverlapHints from './addOverlapHints';
 import styles from './AvailableTimes.css';
 import weekAt from './weekAt';
 
+const leftArrowSvg = (
+  <svg height="24" viewBox="0 0 24 24" width="24">
+    <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
+    <path d="M0-.5h24v24H0z" fill="none"/>
+  </svg>
+);
+
+const rightArrowSvg = (
+  <svg height="24" viewBox="0 0 24 24" width="24">
+    <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
+    <path d="M0-.25h24v24H0z" fill="none"/>
+  </svg>
+);
+
 function oneWeekAhead(date) {
   const result = new Date(date.getTime());
   result.setDate(date.getDate() + 7);
@@ -94,12 +108,18 @@ export default class AvailableTimes extends Component {
       >
         <div className={styles.toolbar}>
           <div className={styles.buttons}>
-            <button onClick={this.handleNavClick.bind(this, -1)}>
-              &lt;
+            <button
+              className={styles.button}
+              onClick={this.handleNavClick.bind(this, -1)}
+            >
+              {leftArrowSvg}
             </button>
             {' '}
-            <button onClick={this.handleNavClick.bind(this, 1)}>
-              &gt;
+            <button
+              className={styles.button}
+              onClick={this.handleNavClick.bind(this, 1)}
+            >
+              {rightArrowSvg}
             </button>
           </div>
           <div className={styles.interval}>
