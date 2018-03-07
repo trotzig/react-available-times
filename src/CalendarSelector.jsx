d6be5c6b412f3f9ed8d87ad4ea5aceb87460f3ca
Henric Trotzig <henric.trotzig@brigade.com>
2018-03-07 16:44:07 +0100

    Fix lints

--- src/CalendarSelector.jsx
+++ src/CalendarSelector.jsx
@@ -4,13 +4,6 @@ import React, { PureComponent } from 'react';
 
 import styles from './CalendarSelector.css';
 
-const downIconSvg = (
-  <svg height="24" viewBox="0 0 24 24" width="24">
-    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
-    <path d="M0-.75h24v24H0z" fill="none" />
-  </svg>
-);
-
 const checkmarkSvg = (
   <svg height="24" viewBox="0 0 24 24" width="24">
     <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
@@ -107,6 +100,7 @@ export default class CalendarSelector extends PureComponent {
           onClick={this.toggleOpen}
         >
           <svg viewBox="0 0 24 24">
+            {/* eslint-disable max-len */}
             <path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z" />
           </svg>
         </button>
