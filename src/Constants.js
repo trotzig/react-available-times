09c98acc6b92eeeaadb4aa9e3700b00bfe5f04d0
Henric Trotzig <henric.trotzig@brigade.com>
2018-03-07 15:58:22 +0100

    Update height of hour
    To 50 pixels, giving it more space.

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const HOUR_IN_PIXELS = 40;
+export const HOUR_IN_PIXELS = 50;
 export const MINUTE_IN_PIXELS = HOUR_IN_PIXELS / 60;
 export const RULER_WIDTH_IN_PIXELS = 40;
 export const WEEKS_PER_TIMESPAN = 4;
