6acd004ed554a94497159db4991cc3064a484fc7
Henric Trotzig <henric.trotzig@brigade.com>
2018-03-07 16:48:24 +0100

    Fix failing tests

--- src/Ruler.jsx
+++ src/Ruler.jsx
@@ -18,7 +18,7 @@ export default function Ruler({ timeConvention }) {
           style={{ height: HOUR_IN_PIXELS }}
         >
           <div className={styles.inner}>
-            {hour > 0 ? hour : null}
+            {hour !== '00' && hour !== '12am' ? hour : null}
           </div>
         </div>
       ))}
